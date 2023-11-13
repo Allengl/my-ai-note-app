b4819e59014949d381bc9494917a8e5a5e9874d2
guolong <guolong.deng@hand-china.com>
2023-11-13 17:00:20 +0800

    feat: completed home page

---
+++ src/app/notes/layout.tsx
@@ -0,0 +1,18 @@
+import NavBar from "./NavBar"
+
+const Layout = ({
+  children
+}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <>
+      <NavBar />
+      <main className="p-4 max-w-7xl m-auto">
+        {children}
+      </main>
+    </>
+  )
+}
+
+export default Layout
