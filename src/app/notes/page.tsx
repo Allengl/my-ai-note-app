1161586f76a7558be816bb74c7d8d94777c10a76
guolong <guolong.deng@hand-china.com>
2023-11-13 17:43:07 +0800

    feat: add validation

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,9 +1,19 @@
+import { auth } from "@clerk/nextjs";
 import { Metadata } from "next"
+import prisma from "@/lib/db/prisma"
 
-export const metadata:Metadata = {
+export const metadata: Metadata = {
   title: 'SmartNote - notes',
 }
+const NotesPage = async () => {
+  const { userId } = auth();
 
-export default function NotesPage() {
-  return <div>Here will be your notes</div>
+  if (!userId) throw Error('userId undefined');
+
+  const allNotes = await prisma.note.findMany({
+    where: { userId },
+  });
+  return <div>{JSON.stringify(allNotes)}</div>
 }
+
+export default NotesPage
