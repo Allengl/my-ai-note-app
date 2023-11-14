7a23f6f39878ea1b583e9eb8482a21dd84968887
guolong <guolong.deng@hand-china.com>
2023-11-14 17:49:41 +0800

    feat: add chatbot styles

--- src/components/AIChatBox.tsx
+++ src/components/AIChatBox.tsx
@@ -1,10 +1,12 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 import { useChat } from "ai/react"
 import { cn } from '@/lib/utils'
-import { XCircle } from 'lucide-react'
+import { Bot, Trash, XCircle } from 'lucide-react'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { Message } from 'ai'
+import { useUser } from '@clerk/nextjs'
+import Image from 'next/image'
 
 interface AIChatBoxProps {
   open: boolean,
@@ -22,22 +24,68 @@ const AIChatBox: FC<AIChatBoxProps> = ({ open, onClose }) => {
     error,
   } = useChat() //  /api/chat
 
+  const inputRef = useRef<HTMLInputElement>(null)
+  const scrollRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+    }
+  }, [messages])
+
+  useEffect(() => {
+    if (open) {
+      inputRef.current?.focus()
+    }
+  }, [open])
+
+  const lastMessageIsUser = messages[messages.length - 1]?.role === 'user'
+
   return (
     <div className={cn("bottom-0 right-0 z-10 w-full max-w-[500px] p-1 xl:right-36", open ? "fixed" : "hidden")}>
       <button onClick={onClose} className='mb-1 ms-auto block'>
         <XCircle size={30} />
       </button>
       <div className='flex h-[600px] flex-col rounded bg-background border shadow-xl'>
-        <div className='h-full'>
+        <div className='h-full mt-3 px-3 overflow-y-auto' ref={scrollRef}>
           {messages.map((message) => (
             <ChatMessage message={message} key={message.id} />
           ))}
+          {
+            isLoading && lastMessageIsUser && (
+              <ChatMessage
+                message={{ role: 'assistant', content: 'Thinking...' }}
+              />
+            )}
+          {
+            error && (
+              <ChatMessage
+                message={{ role: 'assistant', content: 'Oops, something went wrong. Please try again.' }}
+              />
+            )}
+            { !error && messages.length === 0 && (
+              <div className='flex h-full items-center justify-center gap-3'>
+                <Bot/>
+                Ask the AI a question about your notes.
+              </div>
+            )}
         </div>
         <form onSubmit={handleSubmit} className='m-3 flex gap-1'>
+          <Button
+            title='Clear chat'
+            variant='outline'
+            size='icon'
+            className='shrink-0'
+            type='button'
+            onClick={() => setMessages([])}
+          >
+            <Trash />
+          </Button>
           <Input
             value={input}
             onChange={handleInputChange}
             placeholder='Say something...'
+            ref={inputRef}
           />
           <Button type='submit'>
             Send
@@ -49,15 +97,33 @@ const AIChatBox: FC<AIChatBoxProps> = ({ open, onClose }) => {
 }
 
 
-const ChatMessage = ({ message: { role, content } }: { message: Message }) => {
+const ChatMessage = ({
+  message: { role, content }
+}: {
+  message: Pick<Message, "role" | "content">
+}) => {
+  const { user } = useUser()
+
+  const isAiMessage = role === 'assistant'
+
   return (
-    <div className='mb-3'>
-      <div>
-        {role}
-      </div>
-      <div>
+    <div className={cn('mb-3 flex items-center',
+      isAiMessage ? "me-5 justify-start" : "justify-end ms-5")}>
+      {isAiMessage && <Bot className='mr-2 shrink-0' />}
+      <p className={cn("whitespace-pre-line rounded-md border px-3 py-2",
+        isAiMessage ? "bg-background" : "bg-primary text-primary-foreground"
+      )}>
         {content}
-      </div>
+      </p>
+      {!isAiMessage && user?.imageUrl && (
+        <Image
+          src={user.imageUrl}
+          alt="user image"
+          width={40}
+          height={40}
+          className='ml-2 rounded-full w-10 h-10 object-cover'
+        />
+      )}
     </div>
   )
 }
