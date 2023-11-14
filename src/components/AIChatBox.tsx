2bebdd2f17ac41c3e819a270af4b7a3c165a4d65
guolong <guolong.deng@hand-china.com>
2023-11-14 11:17:32 +0800

    feat: create AIChatBox.tsx , AIChatButton.tsx

---
+++ src/components/AIChatBox.tsx
@@ -0,0 +1,46 @@
+import React, { FC } from 'react'
+import { useChat } from "ai/react"
+import { cn } from '@/lib/utils'
+import { XCircle } from 'lucide-react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+
+interface AIChatBoxProps {
+  open: boolean,
+  onClose: () => void
+}
+
+const AIChatBox: FC<AIChatBoxProps> = ({ open, onClose }) => {
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    setMessages,
+    isLoading,
+    error,
+  } = useChat()
+
+  return (
+    <div className={cn("bottom-0 right-0 z-10 w-full max-w-[500px] p-1 xl:right-36", open ? "fixed" : "hidden")}>
+      <button onClick={onClose} className='mb-1 ms-auto block'>
+        <XCircle size={30} />
+      </button>
+      <div className='flex h-[600px] flex-col rounded bg-background border shadow-xl'>
+        <div className='h-full'>Messages</div>
+        <form onSubmit={handleSubmit} className='m-3 flex gap-1'>
+          <Input
+            value={input}
+            onChange={handleInputChange}
+            placeholder='Say something...'
+          />
+          <Button type='submit'>
+            Send
+          </Button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default AIChatBox
